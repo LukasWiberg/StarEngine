a91d6bc8909cae42d5b360f0f7cf03674efa2a06
Lukas <lukas.wiberg@hotmail.com>
2021-08-14 19:38:19 +0200

    Added glm, glfw, tinyobj_loader, stb_image. Added viking_room, basic vert and frag shader.

---
+++ StarEngine/compile-shader.bat
@@ -0,0 +1,3 @@
+C:/VulkanSDK/1.2.176.1/Bin/glslc.exe %~dp0/Shaders/a.vert -o %~dp0/Resources/Shaders/a-vert.spv
+C:/VulkanSDK/1.2.176.1/Bin/glslc.exe %~dp0/Shaders/a.frag -o %~dp0/Resources/Shaders/a-frag.spv
+exit 0
